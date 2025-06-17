53459975741a40dac5f8c0cb980eb18385d3266a
Roberto Orlandini <betoorlan@gmail.com>
2025-06-17 14:08:08 -0300

    Add files via upload

---
+++ ProjetoFinalBlueJ/Ponto.java
@@ -0,0 +1,22 @@
+public class Ponto
+{
+    private int x;
+    private int y;
+
+    public Ponto(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    
+    // Métodos Getters
+    public int getX()
+    {
+        return this.x;
+    }
+    
+    public int getY()
+    {
+        return this.y;
+    }
+}
